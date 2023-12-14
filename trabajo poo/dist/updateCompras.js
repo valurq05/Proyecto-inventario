7dd0b93453a587aa4997e89819f8679639bd1824
valurq05 <valentinaurquijo@gmail.com>
2023-12-14 00:13:50 -0500

    holap

--- trabajo poo/dist/updateCompras.js
+++ trabajo poo/dist/updateCompras.js
@@ -1,19 +1,19 @@
 function mostrarFormularioModCompras(btnEditar) {
+    var compraId = btnEditar.getAttribute("cod_compra");
     var proId = btnEditar.getAttribute("pro_id");
-    var proTienda_id = btnEditar.getAttribute("pro_tienda_id");
-    var proCantTienda = btnEditar.getAttribute("pro_tienda_cant");
-    var proTiendaRepartidor = btnEditar.getAttribute("persona_entrega_producto");
-    var fechaEntregaProTienda = btnEditar.getAttribute("fecha_entrega_producto");
+    var prov_id = btnEditar.getAttribute("prov_id");
+    var precio_proveedor = btnEditar.getAttribute("compra_pre_proveedor");
+    var cant_compra= btnEditar.getAttribute("com_cantidad");
 
-    document.getElementById("proTien-pro-id").value = proTienda_id;
-    document.getElementById("proTien-select-pro").value = proId; 
-    document.getElementById("proTien-pro-cant").value = proCantTienda;
-    document.getElementById("proTien-pro-repartidor").value = proTiendaRepartidor;
-    document.getElementById("proTien-pro-fechaLLegada").value = fechaEntregaProTienda;
+    document.getElementById("compra-codigo").value = compraId;
+    document.getElementById("compra-producto-select").value = proId; 
+    document.getElementById("compra-proveedor-select").value = prov_id;
+    document.getElementById("compra-precio-proveedor").value = precio_proveedor;
+    document.getElementById("compra-cantidad").value = cant_compra;
 
-    document.getElementById("FormModifyProTienda").style.display = "block";
+    document.getElementById("FormModifyCompra").style.display = "block";
 }
 
-function cerrarFormularioModProTienda() {
-    document.getElementById("FormModifyProTienda").style.display = "none";
+function cerrarFormularioModComprass() {
+    document.getElementById("FormModifyCompra").style.display = "none";
 }
