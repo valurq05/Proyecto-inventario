ec1e95ce268aee20a17bf8c429f0c2f704f2ef5e
wilmerx5 <wilmercampos2004@gmail.com>
2023-12-14 02:13:25 -0500

    jkkdas

---
+++ trabajo poo/mailer/mail.php
@@ -0,0 +1,52 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require("Exception.php");
+require("PHPMailer.php");
+require("SMTP.php");
+
+//Create an instance; passing `true` enables exceptions
+function sendMail($email,$subject,$body){
+    try {
+        $mail = new PHPMailer(true);
+        //Server settings
+        $mail->SMTPDebug =0;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'inventario@developersvalhalla.com';                     //SMTP username
+        $mail->Password   = 'SofiaInventario.2';                               //SMTP password
+        $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+        //Recipients
+        $mail->setFrom('inventario@developersvalhalla.com', 'Inventario App');
+        $mail->addAddress($email);     //Add a recipient
+                //Name is optional
+        // $mail->addReplyTo('info@example.com', 'Information');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+    
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+        $mail->send();
+        // echo 'Message has been sent';
+    } catch (Exception $e) {
+        // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        return "error";
+    }
+ }
+
+
+?>
